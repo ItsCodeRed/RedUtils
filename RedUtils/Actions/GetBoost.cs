3fb8defa4ec50aa14b659f04d8547692b9f1749f
NicEastvillage <nicoesterby@gmail.com>
2022-01-26 16:09:31 +0100

    Add GetBoost constructor that picks from IEnumerable<Boost>

--- RedUtils/Actions/GetBoost.cs
+++ RedUtils/Actions/GetBoost.cs
@@ -1,4 +1,6 @@
-﻿using RedUtils.Math;
+﻿using System.Collections;
+using System.Collections.Generic;
+using RedUtils.Math;
 
 namespace RedUtils
 {
@@ -96,6 +98,33 @@ namespace RedUtils
 			_initiallyInterruptible = interruptible;
 
 			BoostIndex = boostIndex;
+			ChosenBoost = Field.Boosts[BoostIndex];
+			DriveAction = new Drive(car, ChosenBoost.Location, Car.MaxSpeed, true, ChosenBoost.IsLarge);
+		}
+
+		/// <summary>Initializes a GetBoost action which will go for the soonest reachable boost of the supplied boosts</summary>
+		/// <param name="boosts">Which boosts to consider</param>
+		/// <param name="interruptible">Whether or not this action can be interrupted</param>
+		public GetBoost(Car car, IEnumerable<Boost> boosts, bool interruptible = true)
+		{
+			Finished = false;
+			Interruptible = interruptible;
+			_initiallyInterruptible = interruptible;
+
+			float fastestEta = 999;
+			// Loop through the given boost pads, and finds the one we can get to soonest
+			foreach (Boost boost in boosts)
+			{
+				// Calculates how long it will take to get the boost
+				float eta = Drive.GetEta(car, boost.Location);
+				// If we can get there fastest, and it will be active when we get there, we choose it as our new fastest!
+				if (eta < fastestEta && (boost.IsActive || boost.TimeUntilActive < eta))
+				{
+					fastestEta = eta;
+					BoostIndex = boost.Index;
+				}
+			}
+
 			ChosenBoost = Field.Boosts[BoostIndex];
 			DriveAction = new Drive(car, ChosenBoost.Location, Car.MaxSpeed, true, ChosenBoost.IsLarge);
 		}
